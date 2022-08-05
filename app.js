86804cfa34dd572d679068af3e0c8a119e41b0cb
PAULO ALISSON <alyssonbbmp@gmail.com>
2022-08-04 22:30:59 -0300

    Ajustes

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var produtos = require('./routes/produtos');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const produtos = require('./routes/produtos');
 
-var app = express();
+const app = express();
 
-var mysql = require('mysql');
+const mysql = require('mysql');
 pool = mysql.createPool({
   host: 'http://localhost:3306',
   user: 'root',
@@ -36,7 +36,7 @@ app.use('/produtos', produtos);
 
 
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
